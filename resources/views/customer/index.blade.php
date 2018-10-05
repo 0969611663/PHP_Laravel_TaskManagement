99f7bff6c245c1dcda04f3d5a5f9ec84774fce37
tu <nguyen.tu.st73@gmail.com>
2018-10-05 09:33:14 +0700

    ..

---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,39 @@
+@extends('customer.layout')
+
+@section('title')
+    Customer manage
+@endsection
+
+@section('content')
+    <div class="col-12">
+        <div class="row">
+            <h1>Danh Sách Khách Hàng</h1>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Tên khách hàng</th>
+                    <th scope="col">Ngày Sinh</th>
+                    <th scope="col">Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                @if(count($customers) == 0)
+                    <tr>
+                        <td colspan="4">Không có dữ liệu</td>
+                    </tr>
+                @else
+                    @foreach($customers as $key => $customer)
+                        <tr>
+                            <th scope="row">{{ ++$key }}</th>
+                            <td>{{ $customer['name'] }}</td>
+                            <td>{{ $customer['bod'] }}</td>
+                            <td>{{ $customer['email'] }}</td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
